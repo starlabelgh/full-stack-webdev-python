ad7b3aecd793234a88d31e9620f7bee1fc7cf367
starlabelgh <hello@starlabel.co>
2022-06-16 08:37:22 +0000

    javascript

---
+++ whileloop.js
@@ -0,0 +1,65 @@
+// for loop
+
+// const { LEGAL_TCP_SOCKET_OPTIONS } = require("mongodb");
+
+// var age = 18
+// var gender = "female"
+
+// if (age >= 21){
+//     console.log("Akua is not a teenager");
+// }else if (age >= 18 && gender == "female") {
+//     console.log("Akua is a teenager");
+// }else {
+//     console.log("I don't know Akua's age")
+// }
+
+
+//  while loop
+
+// var x = 0;
+
+// while (x < 5) {
+//     console.log("x is currently: " +x)
+
+//     if(x===3){
+//         console.log("x is equal to three")
+//         break;
+//     }
+
+//     console.log("x is still less than 5, adding 1 to x")
+
+//     x = x+1
+// }
+
+//print even numbers from one to 10
+// var evenNumber = 1;
+
+// while (evenNumber <= 10) {
+//     if (evenNumber%2 === 0) {
+//         console.log(evenNumber);
+//     }
+//     evenNumber = evenNumber+1;
+// }
+
+//for loops
+
+// var word = "ABCDEFGHIJK"
+
+// for (var i = 0; i < word.length; i = i++){
+//     console.log(word[i]);
+// }
+
+//for loop print odd numbers to 25
+for (var i = 0; i < 25; i++) {
+    if (i%2 !==0){
+        console.log(i)
+    }
+}
+
+//while loop print odd number to 25
+// var x = 1
+// while (x < 25){
+//     if(x%2 !==0){
+//         console.log(x);
+//     }
+// }
