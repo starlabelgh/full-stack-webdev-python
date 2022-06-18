10d8325b6e1788454ce15941a46fb84a41b7ad96
starlabelgh <hello@starlabel.co>
2022-06-18 15:38:53 +0000

    updated the four game app

--- connect4game.js
+++ connect4game.js
@@ -35,3 +35,85 @@ function checkBottom(colIndex){
 function colorMatchCheck(one, two, three, four){
     return (one === two && one === three && one === four && one != 'rgb(128, 128, 128)' && one != undefined)
 }
+
+
+//check for horizontal win
+function horizontalWinCheck(){
+    for (var row = 0; row < 6; row++) {
+        for (var col = 0; col < 4; col++) {
+            if (colorMatchCheck(returnColor(row,col) ,returnColor(row,col+2))){
+                console.log('horiz');
+                return true;
+            }else {
+                continue;
+            }
+        }
+    }
+}
+
+//check for vertical wins
+function verticalWinCheck(){
+    for (var row = 0; row < 7; row++) {
+        for (var col = 0; col < 3; col++) {
+            if (colorMatchCheck(returnColor(row,col) ,returnColor(row+1,col), returnColor(row,col))){
+                console.log('vertical');
+                reportWin(row,col);
+                return true;
+            }else {
+                continue;
+            }
+        }
+    }
+}
+
+//diagonal win check
+function diagonalWinCheck(){
+    for (var row = 0; row < 5; col++) {
+        for (var col = 0; col < 7; row++) {
+            if (colorMatchCheck(returnColor(row,col) ,returnColor(row+1,col+1), returnColor(row+2,col+2), returnColor(row+3,col+3))){
+                console.log('diag');
+                reportWin(row,col);
+                return true;
+            }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1), returnColor(row-2,col+2), returnColor(row-3,col+3), returnColor(row-4,col+4))){
+                console.log('diag');
+                reportWin(row,col);
+                return true;
+            }else {
+                continue;
+            }
+        }
+    }
+}
+
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1Color;
+
+$('h3').text(player1+" it is your turn, pick a column to drop in!")
+
+$('.board button').on('click', function(){
+
+    var col = $(this).closest('td').index();
+
+    var bottomAvail = checkBottom(col);
+
+    changeColor(bottomAvail,col,currentColor);
+
+    if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()) {
+        $('h1').text(currentName+"You have won!")
+        $('h3').fadeOut('fast');
+        $('h2').fadeOut('fast');
+    }
+
+    currentPlayer = currentPlayer * -1;
+
+    if (currentPlayer === 1) {
+        currentName = player1;
+        $('h3').text(currentName+" it is your turn.")
+        currentColor = player1Color
+    }else {
+        currentName = player2;
+        $('h3').text(currentName+ " it is your turn")
+        currentColor = player2Color;
+    }
+})
