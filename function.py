3ed7a1dcc63508c1aec26bf699665dfb2b27e0c5
starlabelgh <hello@starlabel.co>
2022-06-18 22:38:35 +0000

    python dictionatiries

---
+++ function.py
@@ -0,0 +1,25 @@
+from cgitb import reset
+from unittest import result
+
+
+def addnNum(num1, num2):
+    if type(num1) == type(num2) == type(10):
+        return num1+num2
+    else:
+        return "Sorry, I need Intergers only!"
+    
+result = addnNum("3","20")
+print(result)
+
+
+#Lamda Expression
+
+#Filter function
+myList = [1,2,3,4,5,6,7,8]
+
+# def even_bool(num):
+#     return num%2 == 0
+# evens = filter(even_bool,myList)
+
+evens = filter(lamda num:num%2 == 0, myList)
+print(list(evens))
