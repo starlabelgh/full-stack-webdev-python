f6fa8cae13e8d2f13dda451f5b251f78c3d95b01
starlabelgh <hello@starlabel.co>
2022-06-18 22:11:21 +0000

    python dictionatiries

--- dictionaries.py
+++ dictionaries.py
@@ -18,3 +18,28 @@ x.add('Kofi')
 x.add(True)
 
 print(x)
+
+s = 'django'
+print(s[0])
+print(s[-1])
+print(s[:4])
+print(s[1:4])
+print(s[4:])
+print(s[::-1])
+
+l = [3,7,[1,4,'hello']]
+l[2][2] = 'goodbye' 
+print(l)
+
+d1 = {'simple_key':'hello'}
+d2 = {'k1':{'k2':'hello'}}
+#d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+
+print(d1['simple_key'])
+print(d2['k1']['k2'])
+#print(d3['k1'][0]['nested_key'][1][0])
+
+age = 4
+name = "Sammy"
+
+print("Hello my dog's name is {a} and he is {b} years old.".format(a=age,b=name))
